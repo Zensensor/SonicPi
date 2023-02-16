cea06dd47b6795b8b06453233681eb4ad2e4b33d
Zensensor <124623915+Zensensor@users.noreply.github.com>
2023-02-16 10:33:25 -0800

    Update ChallengeB.rb

--- ChallengeB.rb
+++ ChallengeB.rb
@@ -34,18 +34,21 @@ sample :drum_cymbal_hard
 sample :drum_cowbell, amp: 1.5
 #But also play the snare, kick, and cymbal together
 
-live_loop :one do
-  sample :drum_snare_soft
-  sleep 2
+sleep 24
+
+live_loop :drum1 do
+  4.times do
+    sample :drum_splash_hard
+    sleep 2
+  end
+  stop
 end
 
-live_loop :two do
-  sample :drum_heavy_kick
-  sleep 1
-end
-
-live_loop :three do
-  sample :drum_cymbal_closed
-  sleep 0.5
+live_loop :drum2 do
+  16.times do
+    sample :drum_cymbal_hard
+    sleep 0.5
+  end
+  stop
 end
 
