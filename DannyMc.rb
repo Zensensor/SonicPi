2c7530b5b4ae6f282ab4d0e6070c4d56925c88c9
Zensensor <124623915+Zensensor@users.noreply.github.com>
2023-02-23 11:37:50 -0800

    Update DannyMc.rb

--- DannyMc.rb
+++ DannyMc.rb
@@ -1,69 +1,105 @@
 #Danny - Minecraft
 use_bpm 80
 use_synth :hollow
-
-play :G4
-sleep 1
-play :A4
-sleep 1
-play :B4
-sleep 1
-play :C5
-sleep 1
-play :G4
-sleep 1
-play :A4
-sleep 1
-play :B4
-sleep 1
-play :D5
-sleep 1
-play :C5
-sleep 1
-play :B4
-sleep 1
-play :C5
-sleep 1
-play :D5
-sleep 1
-#Intro Done
-play :G4
-sleep 3
-play :G4
-play :C4
-sleep 1
-play :A4
-sleep 1
-play :B4
-play :E4
-sleep 1
-play :G4
-sleep 1
-#Measure
-play :E4
-play :C4
-sleep 1
-play :D4
-sleep 1
-play :G4
-play :E4
-sleep 1
-play :E4
-sleep 1
-#Measure
-play :D4
-sleep 1
-play :E4
-sleep 0.5
-play :D4
-sleep 0.5
-play :C4
-sleep 1
-play :D4
-sleep 1
-#Measure
-play :E4
-play :C2
-sleep 2
-play :C4
-play :G4
+live_loop :one do
+  1.times do
+    play :G4
+    sleep 1
+    play :A4
+    sleep 1
+    play :B4
+    sleep 1
+    play :C5
+    sleep 1
+    play :G4
+    sleep 1
+    play :A4
+    sleep 1
+    play :B4
+    sleep 1
+    play :D5
+    sleep 1
+    play :C5
+    sleep 1
+    play :B4
+    sleep 1
+    play :C5
+    sleep 1
+    play :D5
+    sleep 1
+    #Intro Done
+    play :G4
+    sleep 3
+    play :G4
+    play :C4
+    sleep 1
+    play :A4
+    sleep 1
+    play :B4
+    play :E4
+    sleep 1
+    play :G4
+    sleep 1
+    #Measure
+    play :E4
+    play :C4
+    sleep 1
+    play :D4
+    sleep 1
+    play :G4
+    play :E4
+    sleep 1
+    play :E4
+    sleep 1
+    #Measure
+    play :D4
+    sleep 1
+    play :E4
+    sleep 0.5
+    play :D4
+    sleep 0.5
+    play :C4
+    sleep 1
+    play :D4
+    sleep 1
+    #Measure
+    play :E4
+    play :C2
+    sleep 2
+    play :C4
+    play :G4
+  end
+  stop
+end
+live_loop :two do
+  1.times do
+    play :B3
+    play :G3
+    sleep 2
+    play :C4
+    sleep 2
+    play :B3
+    play :G3
+    sleep 2
+    play :C4
+    sleep 2
+    play :B3
+    play :G3
+    sleep 2
+    play :A3
+    sleep 2
+    play :C4
+    sleep 3
+    play :B3
+    play :G3
+    sleep 2
+    play :C4
+    sleep 2
+    play :B3
+    play :G3
+    sleep 2
+    play :C4
+    sleep 2
+  end
+  stop
+end
