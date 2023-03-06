c61f3d81b4bfa847957397eaf081fd6d40fc0115
Zensensor <124623915+Zensensor@users.noreply.github.com>
2023-03-06 11:39:53 -0800

    Update McRevamped.rb

--- McRevamped.rb
+++ McRevamped.rb
@@ -1,8 +1,23 @@
 #Minecraft
+mc1 = "C:/Users/david_garcia/Downloads/VillagerForCsSample.wav"
 use_bpm 80
 use_synth :hollow
+
+mC= ["A Minecraft Song", "By Me", "Hope You Enjoy"]
+i = 0
+3.times do
+  puts(mC[i])
+  sleep(2)
+  i = i + 1
+end
+
+with_fx :bpf do
+  sample mc1
+end
+
 live_loop :one do
-  2.times do
+  1.times do
+    sleep 15
     play :G4
     sleep 1
     play :A4
@@ -29,14 +44,44 @@ live_loop :one do
     sleep 1
     play :G4
     sleep 3
-    
-    
+  end
+  stop
+end
+live_loop :one2 do
+  1.times do
+    sleep 30
+    play :G4
+    sleep 1
+    play :A4
+    sleep 1
+    play :B4
+    sleep 1
+    play :C5
+    sleep 1
+    play :G4
+    sleep 1
+    play :A4
+    sleep 1
+    play :B4
+    sleep 1
+    play :D5
+    sleep 1
+    play :C5
+    sleep 1
+    play :B4
+    sleep 1
+    play :C5
+    sleep 1
+    play :D5
+    sleep 1
+    play :G4
+    sleep 3
   end
   stop
 end
 live_loop :two do
   1.times do
-    sleep 15
+    sleep 30
     play :B3
     play :G3
     sleep 2
