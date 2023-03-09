2b5277968e6005aca13640007d140897d69ca02c
Zensensor <124623915+Zensensor@users.noreply.github.com>
2023-03-09 11:29:47 -0800

    Update variable.rb

--- variable.rb
+++ variable.rb
@@ -22,3 +22,15 @@ end
 
 
 puts("HAPPY NEW YEAR!")
+
+
+# the array (list) below is not being used
+rainbowColors = ["red", "orange", "yellow","green", "blue", "indigo", "violet"]
+i = 0
+
+# this code prints all the colors
+7.times do
+  puts(rainbowColors[i])
+  sleep(2)
+  i = i + 1
+end
