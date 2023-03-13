ab91c5e129f0c985a72a9fcaf5fb6d4ed6498b68
Zensensor <124623915+Zensensor@users.noreply.github.com>
2023-03-13 10:00:56 -0700

    Update ChallengeG.rb

--- ChallengeG.rb
+++ ChallengeG.rb
@@ -16,3 +16,20 @@ live_loop :main_theme do
     i = 0
   end
 end
+
+#Seven Nation Army Code 
+# Seven Nation Army by The White Stripes
+use_bpm 120
+use_synth :fm
+use_synth_defaults sustain: 0.25
+Notes = [:e3,:e3,:g3,:e3,:r,:d3,:c3,:b2]
+Sleep = [1.5,0.5,0.75,0.25,0.5,0.5,2,2]
+x = 0
+live_loop :MeasureCounter do
+  8.times do
+    play Notes[x]
+    sleep Sleep[x]
+    x = x + 1
+  end
+  x = 0
+end
