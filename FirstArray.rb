83ac9c813dd3d66b65cd0d20c9479ff9e2241c39
Zensensor <124623915+Zensensor@users.noreply.github.com>
2023-03-20 10:47:14 -0700

    Create FirstArray.rb

---
+++ FirstArray.rb
@@ -0,0 +1,10 @@
+# the array (list) below is not being used
+rainbowColors = ["red", "orange", "yellow","green", "blue", "indigo", "violet"]
+i = 0
+
+# this code prints all the colors
+7.times do
+  puts(rainbowColors[i])
+  sleep(2)
+  i = i + 1
+end
