2e6d7c94382de4c37909d4b00fc0f099b048361b
Zensensor <124623915+Zensensor@users.noreply.github.com>
2023-04-13 11:39:30 -0700

    Update workingdaftp.rb

--- workingdaftp.rb
+++ workingdaftp.rb
@@ -6,11 +6,34 @@ x = 0
 sleeps = [0.5,0.5,0.5,1,1,0.25,0.25]
 y = 0
 
+define :baseline1 do
+  with_fx :ixi_techno, mix: 0.2 do
+    with_fx :lpf, cutoff: 50 + tick * 3 do
+      play_pattern_timed [:fs2, :e2, :d2, :c2, :b1, :a1, :g1, :g1, :a1],
+        [ 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.25, 0.25],
+        release: 0.0, sustain: 0.125, amp: 2
+    end
+  end
+end
 
+define :baseline2 do
+  with_fx :lpf, cutoff: 80 do
+    play_pattern_timed [:a1, :a1, :a1, :a1, :b1, :c2, :r, :c2, :c2, :c2, :d2, :e2, :r, :e2, :e2, :e2, :fs2, :e2, :d2, :c2, :b1, :a1, :g1, :g1, :a1],
+      [1, 1, 1, 0.5, 0.25, 0.25, 1, 1, 1, 0.5, 0.25, 0.25, 1, 1, 1, 1, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.25, 0.25],
+      release: 0.0, sustain: 0.3
+  end
+end
 
-use_synth :dtri
-with_fx :flanger do
-  live_loop :daftp do
+define :baseline3 do
+  with_fx :lpf, cutoff: 80 do
+    play_pattern_timed [:e2, :d3, :e3, :e2, :d3, :e2, :d3, :e2, :d3, :e3, :c2, :c3, :c2, :c3, :c2, :b1, :b1, :a2, :b2, :g2],
+      [0.5, 0.5, 0.5, 0.5, 0.5, 0.25, 0.5, 0.25, 0.25, 0.25, 0.5, 0.5, 0.25, 0.25, 0.5, 0.25, 0.5, 0.25, 0.5, 0.5],
+      release: 0.0, sustain: 0.3
+  end
+end
+
+define :daftsynth1 do
+  with_fx :flanger do
     16.times do
       7.times do
         play beats[x]
@@ -23,6 +46,21 @@ with_fx :flanger do
     end
     stop
   end
+end
+
+
+
+
+
+
+live_loop :synth do
+  use_synth :dtri
+  
+  16.times do
+    daftsynth1
+  end
+  
+  stop
 end
 
 
@@ -59,30 +97,4 @@ end
 live_loop :elec do
   sample :elec_cymbal, beat_stretch: 1, amp: 0.2
   sleep 2
-end
-
-define :baseline1 do
-  with_fx :ixi_techno, mix: 0.2 do
-    with_fx :lpf, cutoff: 50 + tick * 3 do
-      play_pattern_timed [:fs2, :e2, :d2, :c2, :b1, :a1, :g1, :g1, :a1],
-        [ 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.25, 0.25],
-        release: 0.0, sustain: 0.125, amp: 2
-    end
-  end
-end
-
-define :baseline2 do
-  with_fx :lpf, cutoff: 80 do
-    play_pattern_timed [:a1, :a1, :a1, :a1, :b1, :c2, :r, :c2, :c2, :c2, :d2, :e2, :r, :e2, :e2, :e2, :fs2, :e2, :d2, :c2, :b1, :a1, :g1, :g1, :a1],
-      [1, 1, 1, 0.5, 0.25, 0.25, 1, 1, 1, 0.5, 0.25, 0.25, 1, 1, 1, 1, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.25, 0.25],
-      release: 0.0, sustain: 0.3
-  end
-end
-
-define :baseline3 do
-  with_fx :lpf, cutoff: 80 do
-    play_pattern_timed [:e2, :d3, :e3, :e2, :d3, :e2, :d3, :e2, :d3, :e3, :c2, :c3, :c2, :c3, :c2, :b1, :b1, :a2, :b2, :g2],
-      [0.5, 0.5, 0.5, 0.5, 0.5, 0.25, 0.5, 0.25, 0.25, 0.25, 0.5, 0.5, 0.25, 0.25, 0.5, 0.25, 0.5, 0.25, 0.5, 0.5],
-      release: 0.0, sustain: 0.3
-  end
 end
